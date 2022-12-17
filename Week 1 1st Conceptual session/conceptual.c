b59e0d9b8d0496da6a4d3d42f26402d86263dc4d
Ariful Islam <116522948+Newcoderhut@users.noreply.github.com>
2022-12-17 21:46:58 +0300

    Update conceptual.c

--- Week 1 1st Conceptual session/conceptual.c
+++ Week 1 1st Conceptual session/conceptual.c
@@ -1,31 +1,17 @@
 
 
 #include<stdio.h>
-
 int main()
 {
-
    int a,b,c;
-
    printf("Enter 3 integers: ");
-
    scanf("%d %d %d",&a,&b,&c);
-
-
+   
    printf ("These number are = %d %d %d\n",a,b,c);
-
    int sum = a+b+c ;
-
    printf("sum of 3 integer = %d",sum);
-
    float avg = (float)sum/3;
-
    printf("\n The average is = %f",avg);
-
    printf("\nAscii charecter is = %c %c %c",a,b,c);
-
-
-
-
-
+   return 0;
 }
